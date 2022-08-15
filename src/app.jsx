97cf1425da2685bcad948827cc71e96f996fbe7e
zxx as <josun981217@gmail.com>
2022-08-15 18:58:13 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,32 @@
-import './app.css';
+import { useEffect, useState } from "react";
+import "./app.css";
+import Nav from "./components/nav";
+import VideoList from "./components/video_list";
 
 function App() {
-  return <hi>Hello Ang!</hi> ;
+  const [videos, setVideo] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https:/youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyD8hREDHJCoUQQGAegsJyW3m364VZwIubY",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideo(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return (
+    <div className="app">
+      <Nav />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
